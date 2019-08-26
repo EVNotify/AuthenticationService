c8adbc1745574744cd73c3c4aaf79d1d34ab674b
GPlay97 <GPlay97@users.noreply.github.com>
2019-08-26 19:56:51 +0200

    implement register

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcrypt');
 const crypto = require('crypto');
+
 const asyncHandler = require('../utils/asyncHandler');
 const AuthModel = require('../models/Auth');
 const errors = require('../errors.json');
@@ -17,7 +19,23 @@ const getUnusedAKey = asyncHandler(async (_req, res, next) => {
 });
 
 const register = asyncHandler(async (req, res, next) => {
+    if (!req.body.password || req.body.password.length <= 6) return next(errors.INVALID_PASSWORD);
+    if (await (AuthModel.findOne({
+        akey: req.akey
+    }))) {
+        return next(errors.AKEY_ALREADY_REGISTERED);
+    }
+    const token = crypto.randomBytes(10).toString('hex');
 
+    await AuthModel.create({
+        akey: req.params.akey,
+        test: 1,
+        hash: await bcrypt.hash(req.body.password, 10),
+        token
+    });
+    res.json({
+        token
+    });
 });
 
 const login = asyncHandler(async (req, res, next) => {
