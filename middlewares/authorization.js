d2859b07d494918ab533e2ba98aad1eb54f61ca7
GPlay97 <GPlay97@users.noreply.github.com>
2019-09-03 21:47:52 +0200

    fix authorization middleware

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -8,7 +8,7 @@ module.exports = asyncHandler(async (req, res, next) => {
     if (!req.headers.authorization || !req.headers.authorization.length) return next(errors.UNAUTHORIZED);
     axios.post(process.env.AUTHORIZATION_SERVICE, {}, {
         headers: {
-            'Authorization': `Bearer ${req.headers.authorization.split(' ')[1]}`
+            'Authorization': req.headers.authorization
         }
     }).then(() => {
         next();
