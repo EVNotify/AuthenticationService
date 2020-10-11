5ae2cb6c12f5cc324acc10da98d13791bf1a5966
GPlay97 <GPlay97@users.noreply.github.com>
2020-10-11 12:27:01 +0200

    Adds documentation for registration

--- routes/authentication.js
+++ routes/authentication.js
@@ -22,9 +22,52 @@ const authorizationMiddleware = require('@evnotify/middlewares').authorizationHa
  *  {
  *      "akey": "123abc"
  *  }
+ * 
+ * @apiErrorExample {json} AKey generation
+ *  HTTP/1.1 409 Conflict
+ *  {
+ *      "code": 409,
+ *      "message": "An error occured trying to generate new AKey"
+ *  }
  */
 router.get('/akey', authenticationController.getUnusedAKey);
-router.post('/:akey/', authenticationController.register);
+/**
+ * @api {post} /authentication/akey/ Creates a new account
+ * @apiName Register
+ * @apiGroup Authentication
+ * 
+ * @apiDescription This will create a new account for provided AKey.
+ * The AKey acts like a username. Together with a password of your choice this is your account.
+ * You'll retrieve a persistent token that is sensitive, and authenticates you against the API.
+ * The token can be changed with a request with your password.
+ * Note: Do not share the token with people or other 3rd parties you don't trust.
+ * Never share your password.
+ * Note: It automatically creates an API key for you. This will be used to authorize you for future requests.
+ * 
+ * @apiSuccess {String} key your personal API Key for your created account
+ * @apiSuccess {String} token your personal persistent token to authenticate your account against the API (without your password)
+ * 
+ * @apiSuccessExample Your created account
+ *  HTTP/1.1 200 OK
+ *  {
+ *      "key": "12345678abcdefgh",
+ *      "token": "1234567890abcdefghij"
+ *  }
+ * 
+ * @apiErrorExample {json} Invalid password
+ *  HTTP/1.1 400 Bad request
+ *  {
+ *      "code": 400,
+ *      "message": "Password invalid. It must be a string with at least 6 characters"
+ *  }
+ * @apiErrorExample {json} AKey already registered
+ *  HTTP/1.1 409 Conflict
+ *  {
+ *      "code": 409,
+ *      "message": "Provided AKey has already been registered"
+ *  }
+ */
+router.post('/:akey', authenticationController.register);
 router.post('/:akey/login', authorizationMiddleware, authenticationController.login);
 router.post('/:akey/verify', authorizationMiddleware, authenticationController.verifyToken);
 
